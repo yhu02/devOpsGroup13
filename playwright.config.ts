16f60d8fe37d2961c572b53706b0686f85de3ecf
Lars <eedenlars@gmail.com>
2025-03-15 00:45:00 +0100

    integration test!

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   reporter: 'html',
   use: {
-    baseURL: 'https://cloudflare-dns.com',
+    baseURL: 'http://localhost:5173',
     trace: 'on-first-retry',
   },
   projects: [
