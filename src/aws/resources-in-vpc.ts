754c085dd6dbba59d35e6c45cd34cce8547fdbe2
Yvo Hu <yvo.hu@alliander.com>
2025-03-16 15:18:50 +0100

    Develop (#31) (#32)
    * feat: SCRUM-4 fix production environment

--- src/aws/resources-in-vpc.ts
+++ src/aws/resources-in-vpc.ts
@@ -12,6 +12,7 @@ import { ResourceMetaData } from "./resourceMap";
         credentials: {
             accessKeyId: import.meta.env.VITE_AWS_ACCESS_KEY_ID,
             secretAccessKey: import.meta.env.VITE_AWS_SECRET_ACCESS_KEY,
+            sessionToken: import.meta.env.VITE_AWS_SESSION_TOKEN,
         },
     });
 
