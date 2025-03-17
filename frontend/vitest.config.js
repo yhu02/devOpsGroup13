5bd7917da8d563b5761073a1664b0877c2af2f48
Yvo Hu <yvo.hu@alliander.com>
2025-03-17 19:51:30 +0100

    fix: integration test (#49)
    * fix: integration test
    
    * fix: update integration test

--- frontend/vitest.config.js
+++ frontend/vitest.config.js
@@ -1,5 +1,6 @@
 export default {
     test: {
+      exclude: ['node_modules/**', 'integration/**/*.test.ts'], // Exclude node_modules & Playwright tests
       coverage: {
         reporter: ['text', 'lcov'],
       },
