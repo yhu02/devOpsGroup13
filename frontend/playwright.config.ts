9242a02bd399e9e0b5670ac33e05a706fda4ce4a
Yvo Hu <yvo.hu@alliander.com>
2025-03-17 19:43:47 +0100

    fix: integration test

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -1,14 +1,14 @@
 import { defineConfig, devices } from '@playwright/test'
 
 export default defineConfig({
-  testDir: './tests',
+  testDir: './integration',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
   reporter: 'html',
   use: {
-    baseURL: 'http://localhost:5173',
+    baseURL: 'http://localhost:3000',
     trace: 'on-first-retry',
   },
   projects: [
